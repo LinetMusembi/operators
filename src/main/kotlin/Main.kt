8e40f345cc3d9bd0f2394283b6f272c1e38a6d08
linetmusembi <musembilinetmwongeli@gmail.com>
2023-02-08 16:43:58 +0300

    submit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,33 @@
+fun main() {
+    var name = "Ann"
+    println("Hello" +name)
+
+
+    remainder(300 ,50)
+    remainder(17,3)
+
+    addition(34,20)
+    addition(56,200)
+    addition(234,789)
+    addition(675,876)
+
+    FactAboutMe("Linet,IsaConsul")
+
+
+}
+fun remainder(num1:Int,num2:Int){
+    var result = (num1 % num2)
+    println(result)
+
+
+}
+fun addition(num1:Int,num2:Int){
+    var sum = (num1 + num2)
+    println(sum)
+}
+fun FactAboutMe(name:String){
+    var personality=("Linet,IsaConsul")
+    println(personality)
+
+
+}
